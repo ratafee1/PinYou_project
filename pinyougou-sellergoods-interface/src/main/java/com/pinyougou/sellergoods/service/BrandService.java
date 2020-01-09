f5370336dd92a78c83c3e7177f2977afebea5612
ratafee <github@com>
2020-01-10 00:14:21 +0800

    third commit

---
+++ pinyougou-sellergoods-interface/src/main/java/com/pinyougou/sellergoods/service/BrandService.java
@@ -0,0 +1,13 @@
+package com.pinyougou.sellergoods.service;
+
+import com.pinyougou.pojo.TbBrand;
+
+import java.util.List;
+
+/**
+ * 品牌接口
+ * @author bailey
+ */
+public interface BrandService {
+    public List<TbBrand> findAll();
+}
